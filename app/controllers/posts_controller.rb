7aa8cd05e03d556851c4b0c2eb1c3fdf3b93515b
Francois Dumas <dumaslattaque.francois@gmail.com>
2022-05-31 10:59:31 +0200

    Feat - Turbo stream for create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,7 @@ class PostsController < ApplicationController
       if @post.save
         format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
+        format.turbo_stream { flash.now[:notice] = "Post was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
