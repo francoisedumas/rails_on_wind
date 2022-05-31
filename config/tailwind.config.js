6b8f26b1899cb41219fbc791b43b527ebfa3371d
Francois Dumas <dumaslattaque.francois@gmail.com>
2022-05-31 17:52:51 +0200

    Design - Flash message

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,21 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+              opacity: '0',
+              transform: 'translateY(-10px)'
+          },
+          '100%': {
+              opacity: '1',
+              transform: 'translateY(0)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 2s ease-out'
+      },
     },
   },
   plugins: [
